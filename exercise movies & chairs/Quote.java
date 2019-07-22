08df5bcc06ac5effb02b11e86349900a0cf343dc
CepheiMD <dumitru_dz@yahoo.com>
2019-07-22 06:46:21 +0300

    Code for all 3 exercises: Quote from a movie, Quote and details about the movie and chairs & table

---
+++ exercise movies & chairs/Quote.java
@@ -0,0 +1,5 @@
+public class Quote {
+	public static void main (String... args) {
+	System.out.println("I'm going to make him an offer he can't refuse");
+}
+}
---
+++ exercise movies & chairs/QuoteExtended.java
@@ -0,0 +1,6 @@
+public class QuoteExtended {
+	public static void main (String... args) {
+	System.out.println("'I'm going to make him an offer he can't refuse'");
+	System.out.println("Don Vito Corleone, 'The Godfather', 1972");
+}
+}
