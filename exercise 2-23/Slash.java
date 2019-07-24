41bec8291b2571ffeee662b717762035e3334c15
CepheiMD <dumitru_dz@yahoo.com>
2019-07-24 07:12:05 +0300

    wrote code for the exercises 2-12 and 2-23

---
+++ exercise 2-23/Slash.java
@@ -0,0 +1,10 @@
+public class Slash {
+	public static void main(String... args) {
+	System.out.println("  \\/  ");
+	System.out.println(" \\\\// ");
+	System.out.println("\\\\\\///");
+	System.out.println("///\\\\\\");
+	System.out.println(" //\\\\ ");
+	System.out.println("  /\\  ");
+}
+}
